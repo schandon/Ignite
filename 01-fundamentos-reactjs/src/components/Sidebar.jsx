20ead1ad05ff7a894a9beaebffb5e26f65323e74
Alexandre Souza <sp.alexandre26@gmail.com>
2022-07-16 19:44:15 -0300

    sidebar finalizada

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,21 +1,25 @@
 import styles from "./Sidebar.module.css"
-// import { } from "react-icons";
+import { PencilSimpleLine } from "phosphor-react";
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
             <img
-                classeName={styles.cover}
+                className={styles.cover}
                 src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
             />
             
             <div className={styles.profile}>
+                <img className={styles.avatar}
+                    src="https://github.com/schandon.png"
+                />
                 <strong>Alexandre Souza</strong>
-                <p>Web Developer</p>
+                <span>Web Developer</span>
             </div>
             
             <footer>
                 <a href="#">
+                    <PencilSimpleLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
