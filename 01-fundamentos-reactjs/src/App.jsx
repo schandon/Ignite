a6ba02f01d88d4c0b737ca09885d117309cc34e3
Alexandre Souza <sp.alexandre26@gmail.com>
2022-07-17 18:11:18 -0300

    Primeira Parte do Post Pronta

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,10 @@
 import { Hearder } from './components/Header'
-import { Post } from './Post'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
 
 import styles from './App.module.css'
 import './global.css'
-import { Sidebar } from './components/Sidebar'
+
 
 function App() {
   return (
@@ -12,14 +13,7 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post
-            author='Alexandre Souza'
-            content = 'Coe mané, ai é pika'
-          />
-          <Post
-            author='Renata Romanelli'
-            content = 'Nesta data irei estar ocupada'
-          />
+          <Post />
         </main>
       </div>
     </div>
---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,29 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img
+                        className={styles.avatar}
+                        src="https://github.com/schandon.png"
+                    />
+                    <div className={styles.authorInfo}>
+                        <strong>Alexandre Souza</strong>
+                        <span>Web Developer</span>
+
+                    </div>                    
+                </div>
+                <time title="17 de Julho ás  02:07" dataTime='2022-07-17 02:07:45'>Publicado há 1h</time>
+            </header>
+            <div className={styles.content}>
+                <p>Coe mané, ai é pika</p>
+                <p>
+                    <a href=''>#LiveDoCasé</a>{' '}
+                    <a href=''>#SimplismenteIncrivel</a>
+                </p>
+            </div>
+        </article>
+    );
+}
