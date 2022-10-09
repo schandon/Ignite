e29ffe85e990135b1b0dd9486f677caf4c5dfc0c
Alexandre Souza <sp.alexandre26@gmail.com>
2022-10-09 20:58:03 -0300

    Criação das Rotas

--- Trilha Front-end/Modulo 02/02-ignite-timer/src/App.tsx
+++ Trilha Front-end/Modulo 02/02-ignite-timer/src/App.tsx
@@ -1,18 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 import { GobalStyle } from './styles/global'
 import { DefaultTheme } from './styles/themes/default'
 
+
 export function App() {
   return (
     <ThemeProvider theme={DefaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="neutral" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GobalStyle />
     </ThemeProvider>
   )
