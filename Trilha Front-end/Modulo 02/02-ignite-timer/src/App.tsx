8fd30a3f91eeb7993c4791acce90cdb83de8e45f
Alexandre Souza <sp.alexandre26@gmail.com>
2022-10-09 17:27:07 -0300

    finalizando Estrutura da Aplicação

--- Trilha Front-end/Modulo 02/02-ignite-timer/src/App.tsx
+++ Trilha Front-end/Modulo 02/02-ignite-timer/src/App.tsx
@@ -1,16 +1,19 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from "./components/Button";
-import { defaultTheme} from "./styles/themes/default";
+import { Button } from './components/Button'
+
+import { GobalStyle } from './styles/global'
+import { DefaultTheme } from './styles/themes/default'
 
 export function App() {
-
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary'/>
-      <Button variant='secondary'/>
-      <Button variant='danger'/>
-      <Button variant='neutral'/>
+    <ThemeProvider theme={DefaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="neutral" />
       <Button />
-    </ThemeProvider >
+
+      <GobalStyle />
+    </ThemeProvider>
   )
 }
--- Trilha Front-end/Modulo 02/02-ignite-timer/src/components/Button.styles.ts
+++ Trilha Front-end/Modulo 02/02-ignite-timer/src/components/Button.styles.ts
@@ -1,30 +1,18 @@
-import styled, { css } from "styled-components";
+import styled from 'styled-components'
 
-export type ButtonVariant =  'primary' | 'secondary' | 'danger' | 'neutral'
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'neutral'
 
-interface ButtonContainerProps{
-	variant: ButtonVariant;
+interface ButtonContainerProps {
+  variant: ButtonVariant
 }
 
-const buttonVariants = {
-	primary: 'purple',
-	secondary: 'orange',
-	danger: 'red',
-	neutral: 'green'
-};
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 100px;
-	height: 40px;
-	border: 0;
-	border-radius: 10px;
-	margin: 8px;
+  width: 100px;
+  height: 40px;
+  border: 0;
+  border-radius: 10px;
+  margin: 8px;
 
-	background: ${props => props.theme.primary};
-	color: ${props=>props.theme.white};
-/* 
-	${props => {
-	return css`
-		background-color: ${buttonVariants[props.variant]}`
-	}} */
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 `
