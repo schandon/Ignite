7e69bbc94badcc386829653bbd4cbf4ab267f8e8
schandon <sp.alexandre26@gmail.com>
2022-10-14 13:42:10 -0300

    Layout e Rotas Ok

---
+++ Trilha Front-end/Modulo 02/02-ignite-timer/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from "./styles"
+import { Timer, Scroll }  from 'phosphor-react'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { NavLink } from "react-router-dom"
+export function Header() {
+    return (
+        <HeaderContainer>
+            <NavLink to='/'> <img src={logoIgnite} alt="" /> </NavLink>
+            <nav>
+                <NavLink to="/home">
+                    <Timer size={24}/>
+                </NavLink>
+                <NavLink to="/history">
+                    <Scroll size={24}/>
+                </NavLink>
+            </nav>
+        </HeaderContainer>
+    )
+}
