15638e445c38f0e6c039897c5b091612d01731fb
Alexandre <sp.alexandre26@gmail.com>
2023-05-31 08:02:32 -0300

    Criando uploads de arquivos

--- Trilha Back-end/Aluguel de Carro/src/routes/category.routes.ts
+++ Trilha Back-end/Aluguel de Carro/src/routes/category.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import Multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listeCategory";
-const categoryRoutes = Router()
 
+const categoryRoutes = Router()
+const uplaods = Multer({dest: './tmp'});
 
 
 categoryRoutes.post("/", (request, response) => {
@@ -12,5 +14,11 @@ categoryRoutes.post("/", (request, response) => {
 categoryRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request,response);
 })
+
+categoryRoutes.post("/import", uplaods.single("file"),(request, response) => {
+  const {file} = request;
+  console.log(file);
+  return response.send();
+});
 
 export {categoryRoutes};
