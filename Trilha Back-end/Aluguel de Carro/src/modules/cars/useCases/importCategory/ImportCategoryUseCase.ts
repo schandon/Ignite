09536f90ecf48198429cee20f8ea7701f23a9abc
Alexandre <sp.alexandre26@gmail.com>
2023-06-06 08:14:32 -0300

    Inserindo dados do upload no repositorio

--- Trilha Back-end/Aluguel de Carro/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ Trilha Back-end/Aluguel de Carro/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -10,12 +10,12 @@ interface IImportCategory {
 class ImportCategoryUseCase {
   constructor(private categoriesRepository:ICategoryRepository){}
   
-  loadCategories(file: Express.Multer.File){
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{
     return new Promise((resolve, reject) =>{
       const stream = fs.createReadStream(file.path);
       const categories : IImportCategory[] = [];
   
-      const parseFile = csvParse();
+      const parseFile = csvParse({delimiter: ',', relax_quotes: true});
   
       stream.pipe(parseFile);
   
@@ -27,12 +27,22 @@ class ImportCategoryUseCase {
         resolve(categories);
       }).on("error", (err) =>{
         reject(err);
-    })
+      });
+    });
   }
-)};
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
-    console.log(categories)
+    categories.map(async (category)=> {
+      const { name, description } = category;
+
+      const existCategory = this.categoriesRepository.findByName(name);
+      if (!existCategory) {
+        this.categoriesRepository.create({
+          name,
+          description
+        });
+      }
+    });
     
   }
 }
