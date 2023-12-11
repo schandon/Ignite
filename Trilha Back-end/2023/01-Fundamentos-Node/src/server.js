597dbca02b042c0b571bfdbc59a2d0e9041fb853
Alexandre <sp.alexandre26@gmail.com>
2023-12-11 08:03:24 -0300

    Corpo da requisição em JSON (Stream & Buffers)

--- Trilha Back-end/2023/01-Fundamentos-Node/src/server.js
+++ Trilha Back-end/2023/01-Fundamentos-Node/src/server.js
@@ -2,19 +2,34 @@ import http from 'http';
 
 const users = []
 
-const server = http.createServer((request, response)=>{
+const server = http.createServer( async (request, response)=>{
  const { method, url } = request;
  
+ const buffers = []
+
+ for await (const chunck of request){
+   buffers.push(chunck)
+ }
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch{
+    request.body = null
+  }
+  console.log(request.body)
+
  if (method === 'GET' && url ==='/users'){
   return response
     .setHeader('Content-Type', 'application/json')
     .end(JSON.stringify(users))
  }
  if (method === 'POST' && url ==='/users'){
+   
+  const { name, email } = request.body
+  
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com'
+      name,
+      email,
     })
 
     return response.writeHead(201).end()
