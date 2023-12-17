6cf7d0c81d24d175921445b18fb28d661339d891
Alexandre <sp.alexandre26@gmail.com>
2023-12-17 10:28:44 -0300

    Criando arquivo de Routes

--- Trilha Back-end/2023/01-Fundamentos-Node/src/server.js
+++ Trilha Back-end/2023/01-Fundamentos-Node/src/server.js
@@ -1,39 +1,22 @@
 import http from 'http'
-import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
-
-
-const database = new Database
+import { routes } from './routes.js';
 
 const server = http.createServer( async (request, response)=>{
  const { method, url } = request;
 
  await json(request, response)
  
-  console.log(request.body)
+ const route = routes.find(route =>{
+    return route.method === method && route.path === url
+ })
+ 
+ console.log(route)
 
- if (method === 'GET' && url ==='/users'){
-  const user = database.select('users')
-
-  return response.end(JSON.stringify(user));
- }
- if (method === 'POST' && url ==='/users'){
-   
-  const { name, email } = request.body
-  
-    const user = ({
-      id: randomUUID(),
-      name,
-      email,
-    })
-
-    database.insert('users', user)
-
-    return response.writeHead(201).end()
+ if (route){
+  return route.handler(request, response)
  }
   return response.writeHead(404).end('Not Found');
-
 })
 
 server.listen(3333);
